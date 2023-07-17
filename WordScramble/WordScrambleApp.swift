fb688abda76470932cc64af536a209e7f6ab30a4
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-07-17 12:29:26 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Guillermo Ruiz Baires on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
