9447e4ea6da610cf562801407105ee2f3fbbf94a
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-07-18 10:05:47 -0600

    Running code when our app launches
    Using onAppear

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -18,19 +18,24 @@ struct ContentView: View {
                 Section {
                     TextField("Enter your word", text: $newWord)
                         .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
                 }
                 
                 Section {
                     ForEach(usedWords, id: \.self) { word in
-                        Text(word)
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
                     }
                 }
             }
             .navigationTitle(rootWord)
             .onSubmit(addWord)
+            .onAppear(perform: startGame)
         }
     }
-    
+        
     func addWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else { return }
@@ -39,6 +44,17 @@ struct ContentView: View {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "zelda"
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
